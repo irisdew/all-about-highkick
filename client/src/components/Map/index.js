04902896d93ad1b3a81ca781c7e4d365afc57e94
marxl <isl9702@naver.com>
2021-04-09 17:26:19 +0900

    Try different size by depth

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -4,6 +4,24 @@ import CytoscapeComponent from 'react-cytoscapejs';
 import coseBilkent from 'cytoscape-cose-bilkent';
 import two from './2.jpg';
 
+const data = [
+  // node
+  { data: { id: '1', label: '나문희' } },
+  { data: { id: '2', label: '이순재' } },
+  { data: { id: '3', label: '박해미' } },
+  { data: { id: '4', label: '이준하' } },
+  { data: { id: '5', label: '이민호' } },
+  { data: { id: '6', label: '이윤호' } },
+  // edge
+  { data: { id: '102', source: '1', target: '2' } },
+  { data: { id: '304', source: '3', target: '4' } },
+  { data: { id: '104', source: '1', target: '4' } },
+];
+
+const cy_for_rank = Cytoscape({
+  elements: data,
+});
+
 Cytoscape.use(coseBilkent);
 
 export default function Map() {
@@ -46,7 +64,7 @@ export default function Map() {
     {
       selector: '#2',
       style: {
-        shape: 'roundrectangle',
+        // shape: 'roundrectangle',
         'background-image': two,
         'background-fit': 'cover',
       },
