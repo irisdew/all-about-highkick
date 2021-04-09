0419269b60e07bcdc28abd323b42d503ad9f7ff1
marxl <isl9702@naver.com>
2021-04-09 15:30:10 +0900

    Practice react-cytoscapejs

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,19 +1,51 @@
 import React from 'react';
+import Cytoscape from 'cytoscape';
 import CytoscapeComponent from 'react-cytoscapejs';
+import COSEBilkent from 'cytoscape-cose-bilkent';
+
+Cytoscape.use(COSEBilkent);
 
 export default function Map() {
   const elements = [
     { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
     { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
+    { data: { id: 'three', label: 'Node 3' }, position: { x: 100, y: 0 } },
     {
       data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' },
     },
+    {
+      data: {
+        source: 'one',
+        target: 'three',
+        label: 'Edge from Node1 to Node3',
+      },
+    },
   ];
+
+  const layout = { name: 'cose-bilkent' };
 
   return (
     <CytoscapeComponent
       elements={elements}
       style={{ width: '100%', height: '100%' }}
+      //   stylesheet={[
+      //     {
+      //       selector: 'node',
+      //       style: {
+      //         width: 20,
+      //         height: 20,
+      //         shape: 'rectangle',
+      //       },
+      //     },
+      //     {
+      //       selector: 'edge',
+      //       style: {
+      //         width: 15,
+      //       },
+      //     },
+      //   ]}
+      layout={layout}
+      zoom={1}
     />
   );
 }
