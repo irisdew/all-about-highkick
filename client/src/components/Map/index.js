7162942845d5d7eb4bf3be13a390f78ff3f9d5fa
marxl <isl9702@naver.com>
2021-04-09 17:04:28 +0900

    Practice add image on cytoscape

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,49 +1,71 @@
 import React from 'react';
 import Cytoscape from 'cytoscape';
 import CytoscapeComponent from 'react-cytoscapejs';
-import COSEBilkent from 'cytoscape-cose-bilkent';
+import coseBilkent from 'cytoscape-cose-bilkent';
+import two from './2.jpg';
 
-Cytoscape.use(COSEBilkent);
+Cytoscape.use(coseBilkent);
 
 export default function Map() {
   const elements = [
-    { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-    { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-    { data: { id: 'three', label: 'Node 3' }, position: { x: 100, y: 0 } },
+    // node
+    { data: { id: '1', label: '나문희' } },
+    { data: { id: '2', label: '이순재' } },
+    { data: { id: '3', label: '박해미' } },
+    { data: { id: '4', label: '이준하' } },
+    { data: { id: '5', label: '이민호' } },
+    { data: { id: '6', label: '이윤호' } },
+    // edge
+    { data: { id: '102', source: '1', target: '2' } },
+    { data: { id: '304', source: '3', target: '4' } },
+    { data: { id: '104', source: '1', target: '4' } },
+  ];
+
+  const style = [
+    // the stylesheet for the graph
     {
-      data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' },
+      selector: 'node',
+      style: {
+        'background-color': '#666',
+        label: 'data(label)',
+      },
+    },
+
+    {
+      selector: 'edge',
+      style: {
+        width: 3,
+        'curve-style': 'bezier',
+        'line-color': '#ccc',
+        'target-arrow-color': '#ccc',
+        'target-arrow-shape': 'vee',
+        'source-arrow-color': '#ccc',
+        'source-arrow-shape': 'triangle',
+      },
     },
     {
-      data: {
-        source: 'one',
-        target: 'three',
-        label: 'Edge from Node1 to Node3',
+      selector: '#2',
+      style: {
+        shape: 'roundrectangle',
+        'background-image': two,
+        'background-fit': 'cover',
       },
     },
   ];
 
-  const layout = { name: 'cose-bilkent' };
+  const layout = {
+    name: 'cose-bilkent',
+    animate: false,
+    gravityRangeCompound: 1.5,
+    fit: true,
+    tile: true,
+  };
 
   return (
     <CytoscapeComponent
       elements={elements}
       style={{ width: '100%', height: '100%' }}
-      //   stylesheet={[
-      //     {
-      //       selector: 'node',
-      //       style: {
-      //         width: 20,
-      //         height: 20,
-      //         shape: 'rectangle',
-      //       },
-      //     },
-      //     {
-      //       selector: 'edge',
-      //       style: {
-      //         width: 15,
-      //       },
-      //     },
-      //   ]}
+      stylesheet={style}
       layout={layout}
       zoom={1}
     />
