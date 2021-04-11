6099c89462dae7bc6f62e00b18906ccadce46b55
marxl <isl9702@naver.com>
2021-04-11 23:34:51 +0900

    Change onMouseover -> onClick

--- client/src/components/Remocon/index.js
+++ client/src/components/Remocon/index.js
@@ -5,40 +5,32 @@ import { BsCardChecklist } from 'react-icons/bs';
 import { IoHomeOutline, IoGameControllerOutline } from 'react-icons/io5';
 import { GiOrganigram } from 'react-icons/gi';
 
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { home } from '../../actions';
 
-export default function SimpleBottomNavigation() {
+export default function Remocon() {
+  const isSelected = useSelector((state) => state.homeSelect);
   const dispatch = useDispatch();
 
-  const [value, setValue] = React.useState(0);
-
   return (
-    <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(event.target.key);
-        console.log(value);
-      }}
-      showLabels
-    >
+    <BottomNavigation value={isSelected} showLabels>
       <BottomNavigationAction
         key={0}
         label="Home"
-        onMouseOver={() => dispatch(home(0))}
+        onClick={() => dispatch(home(0))}
         icon={<IoHomeOutline />}
       />
 
       <BottomNavigationAction
         key={1}
         label="Test"
-        onMouseOver={() => dispatch(home(1))}
+        onClick={() => dispatch(home(1))}
         icon={<BsCardChecklist />}
       />
       <BottomNavigationAction
         key={2}
         label="Game"
-        onMouseOver={() => dispatch(home(2))}
+        onClick={() => dispatch(home(2))}
         icon={<IoGameControllerOutline />}
       />
       <BottomNavigationAction
