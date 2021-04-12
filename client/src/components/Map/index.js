18ec266445f21b67d659c37488a3a900ead26a99
marxl <isl9702@naver.com>
2021-04-12 17:53:58 +0900

    Add full page modal on character map page

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,12 +1,61 @@
 import React, { useState } from 'react';
 import MapOrigin from './MapOrigin';
 import MapDA from './MapDA';
+import Character from '../Character';
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { MdClose } from 'react-icons/md';
+import Slide from '@material-ui/core/Slide';
 
 export default function Map({ isDA }) {
+  const [open, setOpen] = useState(true);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
+
   return (
     <>
-      {!isDA && <MapOrigin />}
+      {!isDA && <MapOrigin onClick={handleClickOpen} />}
       {isDA && <MapDA />}
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <MdClose />
+            </IconButton>
+            <Typography variant="h6">나문희</Typography>
+          </Toolbar>
+        </AppBar>
+        <Character />
+      </Dialog>
     </>
   );
 }
