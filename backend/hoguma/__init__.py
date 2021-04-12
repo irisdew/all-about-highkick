66aa76703cf9c79e57c00bc787546a5cbb4b6968
bumbum9944 <tong940526@gmail.com>
2021-04-12 19:24:33 +0900

    flask app setting - app factory apply - db setting  - SQLAlchemy setting  - migrate setting

---
+++ backend/hoguma/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+import config
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app, supports_credentials=True)
+
+    # app에 config 적용
+    app.config.from_object(config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
