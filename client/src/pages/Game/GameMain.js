c190121586c516a8c768d7361f986703aa28b11e
marxl <isl9702@naver.com>
2021-04-15 17:40:43 +0900

    Update GameMain

--- client/src/pages/Game/GameMain.js
+++ client/src/pages/Game/GameMain.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { gameNumber, gameDescription, gameBall } from '../../actions';
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
 
 export default function GameMain() {
   const history = useHistory();
+  const dispatch = useDispatch();
+  const [gameData, setGameData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/data/game.json')
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.data);
+        setGameData(response.data);
+      });
+  }, []);
+
+  const handleClick = () => {
+    const gameResult = gameData[Math.floor(Math.random() * gameData.length)];
+    dispatch(gameNumber(gameResult.number));
+    dispatch(gameDescription(gameResult.description));
+    dispatch(gameBall(gameResult));
+  };
 
   const Container = styled.div`
     display: flex;
@@ -14,7 +34,7 @@ export default function GameMain() {
     align-items: center;
     justify-content: center;
     flex-direction: column;
-    background-image: '/images/lottobg.jpg';
+    background-image: url(/images/lottobg.jpg);
     object-fit: cover;
     padding: 0 10px;
   `;
@@ -32,6 +52,7 @@ export default function GameMain() {
         onClick={() => {
           history.push('/game/result');
           // 백엔드 API에서 랜덤으로 데이터 1개 받아옴
+          handleClick();
         }}
       >
         번호 뽑기
