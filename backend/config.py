27f8b6fedecd22cbecb6ef43cdc59700eb0f302d
bumbum9944 <tong940526@gmail.com>
2021-04-15 19:00:54 +0900

    migrate 에러 발생

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
-db = {
+db_info = {
     "user": "root",
     "password": "",
     "host": "localhost",
@@ -6,6 +6,6 @@ db = {
     "database": "hoguma",
 }
 
-SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{db_info['user']}:{db_info['password']}@{db_info['host']}:{db_info['port']}/{db_info['database']}?charset=utf8"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "SECRET KEY"
